b6f5b7f5d81ab267b7e1206d1f0a5241eac0ceb7
HashBang <hashbang173@gmail.com>
2016-05-07 13:03:59 -0400

    add aosp_clark.mk

---
+++ aosp_clark.mk
@@ -0,0 +1,15 @@
+# Inherit 64-bit configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/motorola/clark/device.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aosp_clark
+PRODUCT_BRAND := Android
+PRODUCT_DEVICE := clark
+PRODUCT_MODEL := XT1575
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
